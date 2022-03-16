6ed9c9faa7d3f5108003bcfe76efda8c0b8e0214
Ryan <rsandova@ualberta.ca>
2022-03-16 00:19:26 -0600

    Implement image processing pipeline

---
+++ imageProcessing/imageProcessingPipeline.h
@@ -0,0 +1,65 @@
+#ifndef imageProcessingPipeline_H
+#define imageProcessingPipeline_H
+
+#include <list>
+#include <opencv2/core.hpp>
+#include <memory>
+#include "utils/common.h"
+#include <boost/lockfree/spsc_queue.hpp>
+
+using namespace std;
+
+/**
+ * Represents a stage in the image processing pipeline.
+ */
+class ImageProcessingFilter
+{
+public:
+    virtual ~ImageProcessingFilter() = default;
+    // Override in concrete implementations.
+    virtual cv::Mat operator()(cv::Mat in) = 0;
+};
+
+/**
+ * Represents an output location for processed matrices.
+ */
+class ImageProcessingOutput
+{
+public:
+    virtual ~ImageProcessingOutput() = default;
+    // Override in concrete implementations.
+    virtual void operator()(cv::Mat) = 0;
+};
+
+class ImageProcessingPipeline
+{
+public:
+    ImageProcessingPipeline(ImageQueue &queue);
+
+    /**
+     * Adds a new filter stage to the processing pipeline.
+     *
+     * @example
+     * unique_ptr<ImageProcessingFilter> filter = unique_ptr<ImageProcessingFilter>(new EdgeDetectFilter());
+     * pipeline.registerFilter(move(filter));
+     */
+    void registerFilter(unique_ptr<ImageProcessingFilter> filter);
+
+    /**
+     * Adds a new output location for processed matrices.
+     *
+     * @example
+     * unique_ptr<ImageProcessingOutput> output = unique_ptr<ImageProcessingOutput>(new FileSystemOutput());
+     * pipeline.registerOutput(move(output));
+     */
+    void registerOutput(unique_ptr<ImageProcessingOutput> outputs);
+
+    void run();
+
+private:
+    vector<unique_ptr<ImageProcessingFilter>> filters;
+    vector<unique_ptr<ImageProcessingOutput>> outputs;
+    ImageQueue &imageQueue;
+};
+
+#endif
