35d0ad50e7311cc5e340cc2f1aa08810d6deac7d
Ryan <rsandova@ualberta.ca>
2022-03-29 23:05:12 -0600

    Add extra logging to pipeline

--- src/imageProcessing/standardOutput.cpp
+++ src/imageProcessing/standardOutput.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <stdio.h>
 #include <opencv2/imgcodecs.hpp>
+#include "boost/log/trivial.hpp"
 
 using namespace std;
 void FileSystemOutput::operator()(cv::Mat in)
@@ -11,5 +12,6 @@ void FileSystemOutput::operator()(cv::Mat in)
     time_t now = time(0);
     tm *ltm = localtime(&now);
     strftime(buffer,60,"%b-%d-%G_%H-%M-%S.jpg", ltm);
+    BOOST_LOG_TRIVIAL(info) << "Saving image to: " << buffer;
     cv::imwrite(buffer, in);
 }
